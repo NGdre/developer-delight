383cb740d3cf4c86b9da922fe17707b616780f5b
NGdre <100stepstosuccess@gmail.com>
2026-01-07 18:51:32 +0300

    [FIX]: remove space occupied by comments

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -44,13 +44,6 @@ const siteMetadata = {
     // Please add your .env file and modify it according to your selection
     provider: '',
   },
-  comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: '', // supported providers: giscus, utterances, disqus
-  },
   search: {
     provider: 'kbar', // kbar or algolia
     kbarConfig: {
